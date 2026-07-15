a1bc9134a5e93be3f56d1eed13364e178cae7231
Vryez11 <vryez11@gmail.com>
2026-07-15 16:14:57 +0900

    feat: MealRepository 추가
    - 사용자별 기간 조회 쿼리 getMealsByUserIdAndPeriod 작성
      - JPQL로 작성
    - mealAt start <= mealAt < end 조회로 이중 집계 방지

---
+++ src/main/kotlin/com/dandi/nyummy/meal/repository/MealRepository.kt
@@ -0,0 +1,20 @@
+package com.dandi.nyummy.meal.repository
+
+import com.dandi.nyummy.meal.entity.Meal
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDateTime
+
+interface MealRepository : JpaRepository<Meal, Long> {
+
+    @Query(
+        """
+        select m from Meal as m
+        where m.userId = :userId
+        AND m.isDeleted = false
+        AND m.mealAt >= :start 
+        AND m.mealAt < :end
+    """
+    )
+    fun getMealsByUserIdAndPeriod(userId: Long, start: LocalDateTime, end: LocalDateTime): List<Meal>
+}
