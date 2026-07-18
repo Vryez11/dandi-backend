a073725f0538221ce53e9884f9fa3159b84d7ded
Vryez11 <vryez11@gmail.com>
2026-07-18 14:28:23 +0900

    feat: 일일 식사 내역 및 영양 합계 응답 DTO 작성
    - date
    - meals
    - dailyNutrition : current, target

--- src/main/kotlin/com/dandi/nyummy/meal/dto/DailMealsResponse.kt
+++ src/main/kotlin/com/dandi/nyummy/meal/dto/DailMealsResponse.kt
@@ -1,4 +1,28 @@
 package com.dandi.nyummy.meal.dto
 
-class DailMealsResponse {
-}
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class DailMealsResponse (
+
+    val date: LocalDate,
+    val meals: List<DailyMealResponse>,
+    val dailyNutrition: DailyNutritionResponse
+)
+
+data class DailyMealResponse(
+
+    val mealId: Long,
+    val name: String,
+    val mealAt: LocalDateTime,
+    val calories: Int,
+    val carbs: Int,
+    val protein: Int,
+    val fat: Int
+)
+
+data class DailyNutritionResponse(
+
+    val current: Nutrition,
+    val target: Nutrition
+)
