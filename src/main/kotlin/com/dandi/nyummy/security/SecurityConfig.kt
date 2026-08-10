0cb9eff183243dedbae4c6d37c8275b2907590ac
이지원 <vryez11@gmail.com>
2026-08-10 19:15:50 +0900

    feat: Argon encoder 구현 (#35)
    * feat: ArgonEncoder 환경 변수 추가
    
    - salt길이, hash길이, 병렬 처리, 메모리, 반복
    
    * feat: Argon2Encoder 스프링 빈 등록
    
    * refactor: SecurityConfig에서 ArgonProvider 분리
    
    - ArgonProvider로 인코더 및 properties 제공하도록 수정

--- src/main/kotlin/com/dandi/nyummy/security/SecurityConfig.kt
+++ src/main/kotlin/com/dandi/nyummy/security/SecurityConfig.kt
@@ -3,11 +3,14 @@ package com.dandi.nyummy.security
 import com.dandi.nyummy.security.filter.JwtAuthorizationFilter
 import com.dandi.nyummy.security.jwt.JwtProvider
 import jakarta.servlet.DispatcherType
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.DispatcherTypeRequestMatcher
