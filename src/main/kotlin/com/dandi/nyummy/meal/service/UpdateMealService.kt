cacee7db53469a894a0de16069710b797eefdefc
Vryez11 <vryez11@gmail.com>
2026-08-17 18:00:31 +0900

    docs: UpdateMealService 주석 추가

--- src/main/kotlin/com/dandi/nyummy/meal/service/UpdateMealService.kt
+++ src/main/kotlin/com/dandi/nyummy/meal/service/UpdateMealService.kt
@@ -10,12 +10,24 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class UpdateMealService(private val mealRepository: MealRepository) {
 
+    /**
+     * 식사의 분석 상태를 변경하고 즉시 커밋한다.
+     *
+     * @param meal 상태를 변경할 [Meal]
+     * @param status 변경할 [MealStatus]
+     */
     @Transactional
     fun updateStatus(meal: Meal, status: MealStatus) {
         meal.updateStatus(status)
         mealRepository.save(meal)
     }
 
+    /**
+     * 식사의 영양 정보를 저장하고 즉시 커밋한다.
+     *
+     * @param meal 영양 정보를 저장할 [Meal]
+     * @param nutrition 저장할 [Nutrition]
+     */
     @Transactional
     fun updateNutrition(meal: Meal, nutrition: Nutrition) {
         meal.updateNutrition(nutrition)
