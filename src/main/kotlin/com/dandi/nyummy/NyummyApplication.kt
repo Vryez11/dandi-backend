1f20351225e8c5f8d8e2720e5b8afab4768487bb
Vryez11 <vryez11@gmail.com>
2026-09-22 22:48:34 +0900

    refactor: UserDetailServiceAutoConfiguration 클래스 제외

--- src/main/kotlin/com/dandi/nyummy/NyummyApplication.kt
+++ src/main/kotlin/com/dandi/nyummy/NyummyApplication.kt
@@ -3,11 +3,14 @@ package com.dandi.nyummy
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [UserDetailsServiceAutoConfiguration::class],
+)
 class NyummyApplication
 
 fun main(args: Array<String>) {
